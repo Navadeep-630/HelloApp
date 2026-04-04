20c3f79cf3a641a23adc8e7edeab184ddc233c12
Navadeep-630 <nk7269@srmist.edu.in>
2026-04-04 22:00:32 +0530

    UC3 implementation

--- src/main/java/helloapp/HelloApp.java
+++ src/main/java/helloapp/HelloApp.java
@@ -1,11 +1,10 @@
 package helloapp;
 public class HelloApp {
     public static void main(String[] args) {
+    	String name = "world";
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Please provide a name");
+            name = args[0];
         }
-    }
-}
+            System.out.println("Hello, " + name + "!");
+        } 
+    }   
