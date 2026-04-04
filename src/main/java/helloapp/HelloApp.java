89217b05ace65385db471eff26d376b608e4613b
Navadeep-630 <nk7269@srmist.edu.in>
2026-04-04 21:43:20 +0530

    UC2 implementaion

--- src/main/java/helloapp/HelloApp.java
+++ src/main/java/helloapp/HelloApp.java
@@ -1,6 +1,11 @@
 package helloapp;
-public class HelloApp { 
-	 public static void main(String[] args) { 
-	 System.out.println("HelloApp "); 
-	 }
-	} 
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Please provide a name");
+        }
+    }
+}
